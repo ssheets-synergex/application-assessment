afc3902498b031d76b3e91413faefbd2555a340b
Samantha Sheets <samantha.sheets@synergex.com>
2022-11-03 10:45:47 -0600

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AssessmentPage, HomePage } from "./components";
 
+//TODO: Router wrapped in div?
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coming Soon
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/assessment" element={<AssessmentPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
---
+++ src/components/AssessmentPage.tsx
@@ -0,0 +1,3 @@
+export const AssessmentPage = () => {
+  return <div></div>;
+};
