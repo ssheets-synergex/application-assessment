36669419996958471c1d4860dd253440a05ba29c
Samantha Sheets <samantha.sheets@synergex.com>
2023-03-17 10:13:06 -0600

    wrap App.test.tsx with BrowserRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 test('renders App component', () => {
-  const { container } = render(<App />);
+  const { container } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
 
   const boxes = container.getElementsByClassName('App');
   expect(boxes).toHaveLength(1);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,11 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <Router>
+  <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </Router>,
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
